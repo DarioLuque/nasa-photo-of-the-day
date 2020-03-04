4debce7bd24e4d897742d4f27dae02b5d3fa56e9
Dario Luque <dario.lu92@gmail.com>
2020-03-03 17:49:46 -0800

    imported and added card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 
+import NasaList from "./components/NasaList";
+
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
+
+      <NasaList />
     </div>
   );
 }
